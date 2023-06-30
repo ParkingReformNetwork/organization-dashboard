a73a7c92eb896e62a7807097ab04efcb270b7982
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2023-06-30 16:01:45 -0600

    Allow specifying timestamp with createCountPoint (#35)
    This will be useful for historical data.

--- src/utils.ts
+++ src/utils.ts
@@ -9,7 +9,13 @@ const log = (msg: string): void => {
   console.log(`[${timestamp}] ${msg}`);
 };
 
-const createCountPoint = (measurementName: string, count: number): Point =>
-  new Point(measurementName).intField("count", count).timestamp(NOW_S);
+const createCountPoint = (
+  measurementName: string,
+  count: number,
+  timestampInSeconds: number = NOW_S
+): Point =>
+  new Point(measurementName)
+    .intField("count", count)
+    .timestamp(timestampInSeconds);
 
 export { createCountPoint, log, NOW_S };
