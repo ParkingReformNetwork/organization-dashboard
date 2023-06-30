e097328abe15f68cffdf241dbbddc593a9c99cba
Eric Arellano <ericarellano@me.com>
2023-06-30 17:49:08 -0600

    Split up map projects parsing functions

--- tests/mapProjects.test.ts
+++ tests/mapProjects.test.ts
@@ -5,17 +5,15 @@ import { expect, test } from "@playwright/test";
 import mapProjects from "../src/mapProjects";
 import { createCountPoint } from "../src/utils";
 
-test("parsing maps", async () => {
-  const mandates = await fs.readFile("tests/mocks/mandates-map.csv", "utf8");
-  const parkingRaw = await fs.readFile(
-    "tests/mocks/parking-lot-map.json",
-    "utf8"
-  );
-  const parking = JSON.parse(parkingRaw);
+test("parseCitiesJson", async () => {
+  const raw = await fs.readFile("tests/mocks/parking-lot-map.json", "utf8");
+  const parsed = JSON.parse(raw);
+  const result = mapProjects.parseCitiesJson(parsed);
+  expect(result).toEqual(createCountPoint("parking-lot-map-entries", 52));
+});
 
-  const result = mapProjects.parsePoints(mandates, parking);
-  expect(result).toEqual([
-    createCountPoint("mandates-map-entries", 1552),
-    createCountPoint("parking-lot-map-entries", 52),
-  ]);
+test("parseMandatesCsv", async () => {
+  const csv = await fs.readFile("tests/mocks/mandates-map.csv", "utf8");
+  const result = mapProjects.parseMandatesCsv(csv);
+  expect(result).toEqual(createCountPoint("mandates-map-entries", 1552));
 });
