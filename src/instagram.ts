6fd2046af5ab1fb550e335e5f6f073e1f46552b9
Brittney Trinh <brittneytrinh@g.ucla.edu>
2023-06-30 18:19:53 -0700

    get parsing working

--- src/instagram.ts
+++ src/instagram.ts
@@ -1,13 +1,20 @@
 import { Point } from "@influxdata/influxdb-client";
 import axios from "axios";
+import { JSDOM } from "jsdom";
 
 import { createCountPoint } from "./utils";
 
 const getCurrentPoints = async (): Promise<Point[]> => {
-  const response = await axios.get("https://www.instagram.com/parkingreform/", {
+  const response = await axios.get("https://parkingreform.org/", {
     responseType: "text",
   });
-  console.log(response.data);
+  const dom = new JSDOM(response.data);
+  const document = dom.window.document;
+  const footer = document.querySelector("footer");
+  if (!footer) {
+    throw new Error("Could not find footer!");
+  }
+  console.log(footer.outerHTML);
   return [createCountPoint("instagram-followers", 2000)];
 };
 
