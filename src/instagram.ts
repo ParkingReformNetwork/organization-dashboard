fe73feb2f29bcc7dcd30d589e725635deb05a974
Brittney Trinh <brittneytrinh@g.ucla.edu>
2023-06-30 17:49:15 -0700

    get url

--- src/instagram.ts
+++ src/instagram.ts
@@ -1,8 +1,13 @@
 import { Point } from "@influxdata/influxdb-client";
+import axios from "axios";
 
 import { createCountPoint } from "./utils";
 
 const getCurrentPoints = async (): Promise<Point[]> => {
+  const response = await axios.get("https://www.instagram.com/parkingreform/", {
+    responseType: "text",
+  });
+  console.log(response.data);
   return [createCountPoint("instagram-followers", 2000)];
 };
 
