a983beeb6c787985bd1793c1140b61c281d9a1f6
wilson34679 <wilson34679@ucla.edu>
2023-10-30 17:11:26 -0700

    Mastodon followers & posts

---
+++ src/mastodon.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { Point } from "@influxdata/influxdb-client";
+import { createCountPoint } from "./utils";
+
+const mastodon_api =
+  "https://mastodon.social/api/v1/accounts/lookup?acct=@parkingreform@urbanists.social";
+
+const getCurrentPoints = async (): Promise<Point[]> => {
+  const mastodon_Response = await axios.get(mastodon_api);
+  //console.log(mastodon_Response.data);
+
+  const mastodon_followers_point = createCountPoint(
+    "mastodon-followers-count",
+    mastodon_Response.data.followers_count
+  );
+
+  const mastodon_posts_point = createCountPoint(
+    "mastodon-posts-count",
+    mastodon_Response.data.statuses_count
+  );
+  return [mastodon_followers_point, mastodon_posts_point];
+};
+
+export default {
+  getCurrentPoints,
+};
