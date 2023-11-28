df89c12f9a49f5ec291e8e852bbb82ff908910ba
wilson34679 <wilson34679@ucla.edu>
2023-11-28 11:15:26 -0800

    format

--- tests/linkedin.test.ts
+++ tests/linkedin.test.ts
@@ -6,7 +6,7 @@ import linkedin from "../src/linkedin";
 import { createCountPoint } from "../src/utils";
 
 test("parseCitiesJson", async () => {
-    const raw = await fs.readFile("tests/mocks/linkedin.html", "utf8");
-    const result = linkedin.parseHtml(raw);
-    expect(result).toEqual(createCountPoint("linkedin-followers-count", 746));
-  });
+  const raw = await fs.readFile("tests/mocks/linkedin.html", "utf8");
+  const result = linkedin.parseHtml(raw);
+  expect(result).toEqual(createCountPoint("linkedin-followers-count", 746));
+});
